c1cec17102e37566d5484f1952d208c49bdf244d
CostantinB <costantin@me.com>
2015-09-17 13:02:23 +0200

    Updates

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,104 +1,102 @@
-# Data from http://www.hri.fi/
-
-Thing.create(city_id: 1, lng:25.03604916, lat:60.27070187)
-Thing.create(city_id: 2, lng:25.033214, lat:60.28012898)
-Thing.create(city_id: 3, lng:24.95290702, lat:60.16618801)
-Thing.create(city_id: 4, lng:24.94087803, lat:60.16151998)
-Thing.create(city_id: 5, lng:24.95111099, lat:60.17888902)
-Thing.create(city_id: 6, lng:25.02038896, lat:60.173903)
-Thing.create(city_id: 7, lng:25.03062992, lat:60.171183)
-Thing.create(city_id: 8, lng:24.84252795, lat:60.22510296)
-Thing.create(city_id: 9, lng:25.10257897, lat:60.22066701)
-Thing.create(city_id: 10, lng:25.09942805, lat:60.22214597)
-Thing.create(city_id: 11, lng:25.08820296, lat:60.20160296)
-Thing.create(city_id: 12, lng:25.07920994, lat:60.16948297)
-Thing.create(city_id: 13, lng:24.95288203, lat:60.17156499)
-Thing.create(city_id: 14, lng:25.06234195, lat:60.16433098)
-Thing.create(city_id: 15, lng:25.06302, lat:60.25870302)
-Thing.create(city_id: 16, lng:25.10886294, lat:60.217936)
-Thing.create(city_id: 17, lng:25.02818504, lat:60.26579)
-Thing.create(city_id: 18, lng:25.02153093, lat:60.26486696)
-Thing.create(city_id: 19, lng:25.07644499, lat:60.29833202)
-Thing.create(city_id: 20, lng:25.09512999, lat:60.27558801)
-Thing.create(city_id: 21, lng:25.074618, lat:60.31898002)
-Thing.create(city_id: 22, lng:25.06271794, lat:60.31059904)
-Thing.create(city_id: 23, lng:25.113602, lat:60.348278)
-Thing.create(city_id: 24, lng:24.84023899, lat:60.31902904)
-Thing.create(city_id: 25, lng:24.97700495, lat:60.27563103)
-Thing.create(city_id: 26, lng:24.98778601, lat:60.29004197)
-Thing.create(city_id: 27, lng:25.04043906, lat:60.35486205)
-Thing.create(city_id: 28, lng:25.06425494, lat:60.35074204)
-Thing.create(city_id: 29, lng:25.06611006, lat:60.36153601)
-Thing.create(city_id: 30, lng:25.06139702, lat:60.35943802)
-Thing.create(city_id: 31, lng:25.07697506, lat:60.27875799)
-Thing.create(city_id: 32, lng:25.11609196, lat:60.30307603)
-Thing.create(city_id: 33, lng:25.11401403, lat:60.30143898)
-Thing.create(city_id: 34, lng:24.86399797, lat:60.32758595)
-Thing.create(city_id: 35, lng:25.07703396, lat:60.342118)
-Thing.create(city_id: 36, lng:25.07682296, lat:60.34404404)
-Thing.create(city_id: 37, lng:25.07676099, lat:60.34526)
-Thing.create(city_id: 38, lng:25.09829297, lat:60.35061702)
-Thing.create(city_id: 39, lng:24.78534199, lat:60.29056703)
-Thing.create(city_id: 40, lng:24.78964202, lat:60.28850702)
-Thing.create(city_id: 41, lng:25.09258805, lat:60.32507703)
-Thing.create(city_id: 42, lng:25.09551403, lat:60.322887)
-Thing.create(city_id: 43, lng:25.108935, lat:60.24685099)
-Thing.create(city_id: 44, lng:25.07774394, lat:60.32715798)
-Thing.create(city_id: 45, lng:24.87665, lat:60.34718401)
-Thing.create(city_id: 46, lng:24.83575205, lat:60.34387604)
-Thing.create(city_id: 47, lng:25.09119892, lat:60.27137899)
-Thing.create(city_id: 48, lng:25.05760703, lat:60.36285598)
-Thing.create(city_id: 49, lng:25.05768794, lat:60.36442296)
-Thing.create(city_id: 50, lng:25.03374495, lat:60.359698)
-Thing.create(city_id: 51, lng:24.92927895, lat:60.28090599)
-Thing.create(city_id: 52, lng:24.68253704, lat:60.13382298)
-Thing.create(city_id: 53, lng:24.69122205, lat:60.141844)
-Thing.create(city_id: 54, lng:24.741789, lat:60.14755601)
-Thing.create(city_id: 55, lng:24.65481906, lat:60.15044797)
-Thing.create(city_id: 56, lng:24.64268995, lat:60.15823496)
-Thing.create(city_id: 57, lng:24.629065, lat:60.16197904)
-Thing.create(city_id: 58, lng:24.66757396, lat:60.16046998)
-Thing.create(city_id: 59, lng:24.70814493, lat:60.16301202)
-Thing.create(city_id: 60, lng:24.76344603, lat:60.16577396)
-Thing.create(city_id: 61, lng:24.78926002, lat:60.16242296)
-Thing.create(city_id: 62, lng:24.58330102, lat:60.17066705)
-Thing.create(city_id: 63, lng:24.66135306, lat:60.17396298)
-Thing.create(city_id: 64, lng:24.66135306, lat:60.17396298)
-Thing.create(city_id: 65, lng:24.73091805, lat:60.17047202)
-Thing.create(city_id: 66, lng:24.65147794, lat:60.203195)
-Thing.create(city_id: 67, lng:24.57324404, lat:60.17985799)
-Thing.create(city_id: 68, lng:24.59519792, lat:60.18209602)
-Thing.create(city_id: 69, lng:24.75649503, lat:60.19806898)
-Thing.create(city_id: 70, lng:24.81362805, lat:60.21007698)
-Thing.create(city_id: 71, lng:24.63143801, lat:60.216697)
-Thing.create(city_id: 72, lng:24.63012001, lat:60.22541399)
-Thing.create(city_id: 73, lng:24.70624607, lat:60.22500897)
-Thing.create(city_id: 74, lng:24.81170294, lat:60.22078905)
-Thing.create(city_id: 75, lng:24.81266996, lat:60.23202002)
-Thing.create(city_id: 76, lng:24.57366902, lat:60.25412297)
-Thing.create(city_id: 77, lng:24.80912307, lat:60.23625801)
-Thing.create(city_id: 78, lng:24.68979594, lat:60.24025196)
-Thing.create(city_id: 79, lng:24.77217896, lat:60.23889702)
-Thing.create(city_id: 80, lng:24.83117203, lat:60.24062196)
-Thing.create(city_id: 81, lng:24.77805203, lat:60.24962001)
-Thing.create(city_id: 82, lng:24.77805203, lat:60.24962001)
-Thing.create(city_id: 83, lng:24.77805203, lat:60.24962001)
-Thing.create(city_id: 84, lng:24.76883397, lat:60.25219903)
-Thing.create(city_id: 85, lng:24.71967495, lat:60.274769)
-Thing.create(city_id: 86, lng:24.75042997, lat:60.27860602)
-Thing.create(city_id: 87, lng:24.74048596, lat:60.28593804)
-Thing.create(city_id: 88, lng:24.71744104, lat:60.32751601)
-Thing.create(city_id: 89, lng:24.85269602, lat:60.29132602)
-Thing.create(city_id: 90, lng:25.09374993, lat:60.35136198)
-Thing.create(city_id: 91, lng:24.857273, lat:60.29828697)
-Thing.create(city_id: 92, lng:24.98228896, lat:60.29532299)
-Thing.create(city_id: 93, lng:24.98228896, lat:60.29532299)
-Thing.create(city_id: 94, lng:25.04939301, lat:60.24636098)
-Thing.create(city_id: 95, lng:24.98562607, lat:60.147471)
-Thing.create(city_id: 96, lng:24.903446, lat:60.21021997)
-Thing.create(city_id: 97, lng:25.009156, lat:60.25216699)
-Thing.create(city_id: 98, lng:24.88786798, lat:60.16361603)
-Thing.create(city_id: 99, lng:24.93430896, lat:60.22173704)
-Thing.create(city_id: 100, lng:24.98820095, lat:60.18060803)
-Thing.create(city_id: 101, lng:25.00830305, lat:60.24709597)
-Thing.create(city_id: 102, lng:24.90624607, lat:60.17285797)
+Thing.create(city_id: 1,lng:25.03604916,lat:60.27070187)
+Thing.create(city_id: 2,lng:25.033214,lat:60.28012898
+Thing.create(city_id: 3,lng:24.95290702,lat:60.16618801
+Thing.create(city_id: 4,lng:24.94087803,lat:60.16151998
+Thing.create(city_id: 5,lng:24.95111099,lat:60.17888902
+Thing.create(city_id: 6,lng:25.02038896,lat:60.173903
+Thing.create(city_id: 7,lng:25.03062992,lat:60.171183
+Thing.create(city_id: 8,lng:24.84252795,lat:60.22510296
+Thing.create(city_id: 9,lng:25.10257897,lat:60.22066701
+Thing.create(city_id: 10,lng:25.09942805,lat:60.22214597
+Thing.create(city_id: 11,lng:25.08820296,lat:60.20160296
+Thing.create(city_id: 12,lng:25.07920994,lat:60.16948297
+Thing.create(city_id: 13,lng:24.95288203,lat:60.17156499
+Thing.create(city_id: 14,lng:25.06234195,lat:60.16433098
+Thing.create(city_id: 15,lng:25.06302,lat:60.25870302
+Thing.create(city_id: 16,lng:25.10886294,lat:60.217936
+Thing.create(city_id: 17,lng:25.02818504,lat:60.26579
+Thing.create(city_id: 18,lng:25.02153093,lat:60.26486696
+Thing.create(city_id: 19,lng:25.07644499,lat:60.29833202
+Thing.create(city_id: 20,lng:25.09512999,lat:60.27558801
+Thing.create(city_id: 21,lng:25.074618,lat:60.31898002
+Thing.create(city_id: 22,lng:25.06271794,lat:60.31059904
+Thing.create(city_id: 23,lng:25.113602,lat:60.348278
+Thing.create(city_id: 24,lng:24.84023899,lat:60.31902904
+Thing.create(city_id: 25,lng:24.97700495,lat:60.27563103
+Thing.create(city_id: 26,lng:24.98778601,lat:60.29004197
+Thing.create(city_id: 27,lng:25.04043906,lat:60.35486205
+Thing.create(city_id: 28,lng:25.06425494,lat:60.35074204
+Thing.create(city_id: 29,lng:25.06611006,lat:60.36153601
+Thing.create(city_id: 30,lng:25.06139702,lat:60.35943802
+Thing.create(city_id: 31,lng:25.07697506,lat:60.27875799
+Thing.create(city_id: 32,lng:25.11609196,lat:60.30307603
+Thing.create(city_id: 33,lng:25.11401403,lat:60.30143898
+Thing.create(city_id: 34,lng:24.86399797,lat:60.32758595
+Thing.create(city_id: 35,lng:25.07703396,lat:60.342118
+Thing.create(city_id: 36,lng:25.07682296,lat:60.34404404
+Thing.create(city_id: 37,lng:25.07676099,lat:60.34526
+Thing.create(city_id: 38,lng:25.09829297,lat:60.35061702
+Thing.create(city_id: 39,lng:24.78534199,lat:60.29056703
+Thing.create(city_id: 40,lng:24.78964202,lat:60.28850702
+Thing.create(city_id: 41,lng:25.09258805,lat:60.32507703
+Thing.create(city_id: 42,lng:25.09551403,lat:60.322887
+Thing.create(city_id: 43,lng:25.108935,lat:60.24685099
+Thing.create(city_id: 44,lng:25.07774394,lat:60.32715798
+Thing.create(city_id: 45,lng:24.87665,lat:60.34718401
+Thing.create(city_id: 46,lng:24.83575205,lat:60.34387604
+Thing.create(city_id: 47,lng:25.09119892,lat:60.27137899
+Thing.create(city_id: 48,lng:25.05760703,lat:60.36285598
+Thing.create(city_id: 49,lng:25.05768794,lat:60.36442296
+Thing.create(city_id: 50,lng:25.03374495,lat:60.359698
+Thing.create(city_id: 51,lng:24.92927895,lat:60.28090599
+Thing.create(city_id: 52,lng:24.68253704,lat:60.13382298
+Thing.create(city_id: 53,lng:24.69122205,lat:60.141844
+Thing.create(city_id: 54,lng:24.741789,lat:60.14755601
+Thing.create(city_id: 55,lng:24.65481906,lat:60.15044797
+Thing.create(city_id: 56,lng:24.64268995,lat:60.15823496
+Thing.create(city_id: 57,lng:24.629065,lat:60.16197904
+Thing.create(city_id: 58,lng:24.66757396,lat:60.16046998
+Thing.create(city_id: 59,lng:24.70814493,lat:60.16301202
+Thing.create(city_id: 60,lng:24.76344603,lat:60.16577396
+Thing.create(city_id: 61,lng:24.78926002,lat:60.16242296
+Thing.create(city_id: 62,lng:24.58330102,lat:60.17066705
+Thing.create(city_id: 63,lng:24.66135306,lat:60.17396298
+Thing.create(city_id: 64,lng:24.66135306,lat:60.17396298
+Thing.create(city_id: 65,lng:24.73091805,lat:60.17047202
+Thing.create(city_id: 66,lng:24.65147794,lat:60.203195
+Thing.create(city_id: 67,lng:24.57324404,lat:60.17985799
+Thing.create(city_id: 68,lng:24.59519792,lat:60.18209602
+Thing.create(city_id: 69,lng:24.75649503,lat:60.19806898
+Thing.create(city_id: 70,lng:24.81362805,lat:60.21007698
+Thing.create(city_id: 71,lng:24.63143801,lat:60.216697
+Thing.create(city_id: 72,lng:24.63012001,lat:60.22541399
+Thing.create(city_id: 73,lng:24.70624607,lat:60.22500897
+Thing.create(city_id: 74,lng:24.81170294,lat:60.22078905
+Thing.create(city_id: 75,lng:24.81266996,lat:60.23202002
+Thing.create(city_id: 76,lng:24.57366902,lat:60.25412297
+Thing.create(city_id: 77,lng:24.80912307,lat:60.23625801
+Thing.create(city_id: 78,lng:24.68979594,lat:60.24025196
+Thing.create(city_id: 79,lng:24.77217896,lat:60.23889702
+Thing.create(city_id: 80,lng:24.83117203,lat:60.24062196
+Thing.create(city_id: 81,lng:24.77805203,lat:60.24962001
+Thing.create(city_id: 82,lng:24.77805203,lat:60.24962001
+Thing.create(city_id: 83,lng:24.77805203,lat:60.24962001
+Thing.create(city_id: 84,lng:24.76883397,lat:60.25219903
+Thing.create(city_id: 85,lng:24.71967495,lat:60.274769
+Thing.create(city_id: 86,lng:24.75042997,lat:60.27860602
+Thing.create(city_id: 87,lng:24.74048596,lat:60.28593804
+Thing.create(city_id: 88,lng:24.71744104,lat:60.32751601
+Thing.create(city_id: 89,lng:24.85269602,lat:60.29132602
+Thing.create(city_id: 90,lng:25.09374993,lat:60.35136198
+Thing.create(city_id: 91,lng:24.857273,lat:60.29828697
+Thing.create(city_id: 92,lng:24.98228896,lat:60.29532299
+Thing.create(city_id: 93,lng:24.98228896,lat:60.29532299
+Thing.create(city_id: 94,lng:25.04939301,lat:60.24636098
+Thing.create(city_id: 95,lng:24.98562607,lat:60.147471
+Thing.create(city_id: 96,lng:24.903446,lat:60.21021997
+Thing.create(city_id: 97,lng:25.009156,lat:60.25216699
+Thing.create(city_id: 98,lng:24.88786798,lat:60.16361603
+Thing.create(city_id: 99,lng:24.93430896,lat:60.22173704
+Thing.create(city_id: 100,lng:24.98820095,lat:60.18060803
+Thing.create(city_id: 101,lng:25.00830305,lat:60.24709597
+Thing.create(city_id: 102,lng:24.90624607,lat:60.17285797
